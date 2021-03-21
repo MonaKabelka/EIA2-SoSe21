b7c33b72331e2ce8d767df5b995749d5ebe1fd1d
MonaKabelka <mona.kabelka.mk@gmail.com>
2021-03-21 16:06:00 +0100

    Inverted Classroom Test

--- L_01/L01_Inverted/variablen_programm.js
+++ L_01/L01_Inverted/variablen_programm.js
@@ -1,23 +1,15 @@
-let v: number = 1; 
+"use strict";
+let v = 1;
 v = v + 1;
 console.log(v);
-
 //bei v = "1" als string wird 11 ausgegeben 
 //=> Additionsoperator + ist für Zeichenketten ganz anders definiert als für Zahlen
-
 //bei v = true als boolean wird 2 ausgegeben
-
 //bei v = v + "1" funktioniert nicht, da man zu Variable mit Typ number nicht string addieren kann
-
-
-
-let v1: number = 1;
-let v2: number = 4;
-
+let v1 = 1;
+let v2 = 4;
 v2 = v1;
-
 v1 = 10;
-
-console.log(v2); 
+console.log(v2);
 // v2 bleibt immer noch erster wert von v1
-
+//# sourceMappingURL=variablen_programm.js.map
