7f62309c2deea7f299a1e363e15eff961d5734ae
MonaKabelka <mona.kabelka.mk@gmail.com>
2021-03-23 20:13:19 +0100

    poem

--- Aufgabe01/A01_Zufallsgedicht/script/A01_Zufallsgedicht.js
+++ Aufgabe01/A01_Zufallsgedicht/script/A01_Zufallsgedicht.js
@@ -1,2 +1,21 @@
 "use strict";
+var Zufallsgedicht;
+(function (Zufallsgedicht) {
+    let subjekte = ["Harry", "Hermine", "Ron", "Bellatrix", "Snape", "Dumbledore"];
+    let prädikate = ["zaubert", "tötet", "liebt", "appariert", "verflucht", "sucht"];
+    let objekte = ["Zaubertränke", "Werwölfe", "Dementoren", "Hagrid", "Wurmschwanz", "Drachen"];
+    let a;
+    console.log();
+    for (a = 6; a > 0; a--) { //6 bis 1 werden ausgegeben? stimmt das?
+        console.log(a);
+        subjekte = [];
+        prädikate = [];
+        objekte = [];
+        let x = getVerse(subjekte[0], prädikate[0], objekte[0]);
+        console.log(x);
+    }
+    function getVerse(_subjekt, _prädikat, _objekt) {
+        return "Alohomora";
+    }
+})(Zufallsgedicht || (Zufallsgedicht = {}));
 //# sourceMappingURL=A01_Zufallsgedicht.js.map
--- Aufgabe01/A01_Zufallsgedicht/script/A01_Zufallsgedicht.ts
+++ Aufgabe01/A01_Zufallsgedicht/script/A01_Zufallsgedicht.ts
@@ -1,3 +1,23 @@
 namespace Zufallsgedicht {
-    
+   
+    let subjekte: string [] = ["Harry", "Hermine", "Ron", "Bellatrix", "Snape", "Dumbledore"];
+    let prädikate: string [] = ["zaubert", "tötet", "liebt", "appariert", "verflucht", "sucht"];
+    let objekte: string [] = ["Zaubertränke", "Werwölfe", "Dementoren", "Hagrid", "Wurmschwanz", "Drachen"];
+    let a: number;
+
+    console.log()
+
+    for (a = 6; a > 0; a--) { //6 bis 1 werden ausgegeben? stimmt das?
+        console.log(a);
+
+        let x: string = getVerse(subjekte[a], prädikate[a], objekte[a]);
+
+        console.log(x);
+    }
+
+    function getVerse (_subjekt: string, _prädikat: string, _objekt: string) {
+        return "Alohomora";
+        let vers: string = "";
+        Math.random();
+    }
 }
